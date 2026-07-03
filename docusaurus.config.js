53f38148be17d90249fa6616c971549410761008
Varga István <115572434+vargaijanos@users.noreply.github.com>
2026-07-03 13:43:40 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'Open Manuscript Initiative',
   tagline: 'Write naturally. Structure once. Submit anywhere',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.svg',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
