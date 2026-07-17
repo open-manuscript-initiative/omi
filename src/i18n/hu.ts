df76257810a5288df34ec36f6e70688c9622be66
Varga István <115572434+vargaijanos@users.noreply.github.com>
2026-07-17 06:26:03 +0200

    Create hu.ts

---
+++ src/i18n/hu.ts
@@ -0,0 +1,6 @@
+export default {
+  heroTitle: "Open Manuscript Initiative",
+  heroSubtitle: "Írj természetesen. Strukturálj egyszer. Publikálj bárhová.",
+  getStarted: "Kezdés",
+  documentation: "Dokumentáció",
+};
