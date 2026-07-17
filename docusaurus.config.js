9a1ad7d5fa2abd9fa9409b991213af9cc7d712a3
Varga István <115572434+vargaijanos@users.noreply.github.com>
2026-07-17 06:24:27 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,7 +40,16 @@ const config = {
   // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
-    locales: ['en'],
+    locales: ['en', 'hu', 'de'],
+  localeConfigs: {
+    en: {
+      label: 'English',
+    },
+    hu: {
+      label: 'Magyar',
+    },
+    de: {
+      label: 'Deutsch',
   },
 
   presets: [
